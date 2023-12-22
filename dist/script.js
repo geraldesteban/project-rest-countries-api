b43558761de453345d1bd87b22e6b581b547b33b
geraldesteban <wakin.esteban@gmail.com>
2023-12-23 01:28:23 +0800

    add changes

--- dist/script.js
+++ dist/script.js
@@ -3,17 +3,13 @@
 const countriesContainer = document.querySelector(".country-card");
 const renderCountry = function (data) {
   const html = `
-    <article class="country bg-white w-72 py-10">
-        <img class="country-img w-72 rounded-tl-md rounded-tr-md" src="${
-          data.flags.png
-        }" />
+    <article class="country bg-white w-72 rounded-tl-md rounded-tr-md">
+        <img class="country-img w-72 rounded-tl-md rounded-tr-md" src="${data.flags.png}" />
         <div class="country-data p-8">
-        <h2 class="country-name text-xl font-bold mb-5">${data.name.common}</h3>
-            <p class="country-population font-bold mb-1">Population: ${(
-              +data.population / 1000000
-            ).toFixed(1)}</p>
-            <p class="country-region font-bold mb-1">Region: ${data.region}</p>
-            <p class="country-capital font-bold">Capital: ${data.capital}</p>
+        <h2 class="country-name text-xl text-center font-bold mb-5">${data.name.common}</h3>
+            <p class="country-population font-bold mb-1">Population: <span class="font-light">${data.population}</span></p>
+            <p class="country-region font-bold mb-1">Region: <span class="font-light">${data.region}</span></p>
+            <p class="country-capital font-bold">Capital: <span class="font-light">${data.capital}</span></p>
         </div>
     </article>
     `;
@@ -23,18 +19,12 @@ const renderCountry = function (data) {
 
 const getCountryDataAll = async function () {
   try {
-    // Country data
     const resAPI = await fetch(`https://restcountries.com/v3.1/all
     `);
     const resData = await resAPI.json();
-    console.log(resData);
     resData.forEach(rd => {
-      console.log(rd);
       renderCountry(rd);
     });
-    // renderCountry(resData);
-  } catch (err) {
-    console.error(err);
-  }
+  } catch (err) {}
 };
-getCountryDataAll();
+// getCountryDataAll();
